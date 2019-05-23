b2c5acb5af19d42d85f8203156a7632b2b359972
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-22 22:03:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+   
+ 
+   let message;
+  
+   if (ride <= 400) {
+   	return message = 'This one is on me!';
+   }	else if (ride > 400 && ride <= 2500) {
+   return	message = 'I will gladly take your thirty bucks.';
+   } else {
+
+   return message = 'No can do.';
+   } 
+   }
+
+
+function ternaryCheckCity(city){
+ let message;
+ return city === "NYC" ? message = "Ok, sounds good." : message = "No go.";
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+let message;
+switch (tip){
+ case 'generous':
+ return message = "Thank you so much.";
+ case 'not as generous':
+ return message = "Thank you.";
+ default:
+ return message = 'Bye.';
 
-function switchOnCharmFromTip(){
+}   
+
+
   // Write your code here!
 }
